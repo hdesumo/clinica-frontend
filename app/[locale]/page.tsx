1391ff06c857c5a5bd007ba412d607e58854104d
hdesumo <honoredesumo@yahoo.com>
2026-02-17 22:02:47 +0100

    Fix homepage return statement for production build

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,20 +1,7 @@
-import { getDictionary } from "../../i18n/getDictionary";
-import HeroSection from "../../components/HeroSection";
-import StandardsSection from "../../components/StandardsSection";
-
-export default async function Home({
-  params,
-}: {
-  params: Promise<{ locale: string }>;
-}) {
-  const { locale } = await params;
-
-  const dictionary = await getDictionary(locale);
-
+export default function TestPage() {
   return (
-    <>
-      <HeroSection dictionary={dictionary} />
-      <StandardsSection />
-    </>
+    <div style={{ padding: 40 }}>
+      <h1>Clinica Static OK</h1>
+    </div>
   );
 }
