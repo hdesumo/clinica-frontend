cd954f93bb2c7b114c0cac09f8b20a327bed718a
hdesumo <honoredesumo@yahoo.com>
2026-02-17 15:13:46 +0100

    Fix import paths for Vercel build

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,6 +1,6 @@
-import { getDictionary } from "@/i18n/getDictionary";
-import HeroSection from "@/components/HeroSection";
-import StandardsSection from "@/components/StandardsSection";
+import { getDictionary } from "../../i18n/getDictionary";
+import HeroSection from "../../components/HeroSection";
+import StandardsSection from "../../components/StandardsSection";
 
 export default async function Home({
   params,
